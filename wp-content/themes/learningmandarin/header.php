718e6782a10e7916409daafa755b9b4e09aebed8
Chris Schraeder <cwschraeder@gmail.com>
2017-04-12 17:53:31 +0800

    Split Blogpage Add Comments and Function

--- wp-content/themes/learningmandarin/header.php
+++ wp-content/themes/learningmandarin/header.php
@@ -10,18 +10,6 @@
 
     <title>Learning Mandarin</title>
 
-    <!-- Bootstrap core CSS -->
-    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- Custom styles for this template -->
-    <link href="<?php bloginfo('template_directory'); ?>/blog.css" rel="stylesheet">
-
-
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
 <?php wp_head(); ?>
   </head>
 
@@ -38,8 +26,8 @@
 	<div class="blog-masthead">
 		<div class="container">
 			<nav class="blog-nav">
-				<a class="blog-nav-item active" href="#">Home</a>
-				<a class="blog-nav-item" href="#">New features</a>
+			<a class="blog-nav-item active" href="#">Home</a>
+			<?php wp_list_pages( '&title_li=' ); ?>
 			</nav>
 		</div>
 	</div>
--- wp-content/themes/learningmandarin/index.php
+++ wp-content/themes/learningmandarin/index.php
@@ -5,19 +5,40 @@
 		<div class="col-sm-8 blog-main">
 	
 			<?php 
-			if ( have_posts() ) : while ( have_posts() ) : the_post();
-  	
-				get_template_part( 'content', get_post_format() );
-  
-			endwhile; endif; 
+			if ( have_posts() ) : while ( have_posts() ) : the_post();?>
+
+<?php
+if($i == 0) {
+	echo '<div class="split-row">';
+}
+?>
+
+<div class="half">
+
+			<?php get_template_part( 'content', get_post_format() );?>
+</div>
+
+<?php
+$i++;
+if($i == 2) {
+	$i = 0;
+	echo '</div>';
+}
+?>
+
+			<?php endwhile; endif;
 			?>
 
-		</div> <!-- /.blog-main -->
+<?php
+if($i > 0) {
+	echo '</div>';
+}
+?>
 
+		</div> <!-- /.blog-main -->
 
       <?php get_sidebar(); ?> 
 
       </div><!-- /.row -->
-
 
  <?php get_footer(); ?>
